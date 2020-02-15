d0984b25b0e98217505bdcd1b44965ab9e2eeb1b
Manuel Morejon <manuel@mmorejon.io>
2020-02-15 23:04:07 +0100

    Add staging cluster.
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

---
+++ infra/stg/cluster.tf
@@ -0,0 +1,17 @@
+
+# kubernetes cluster
+resource "digitalocean_kubernetes_cluster" "staging" {
+  name    = "staging"
+  region  = "ams3"
+  version = "1.16.6-do.0"
+  tags    = ["staging"]
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    auto_scale = true
+    node_count = 1
+    min_nodes  = 1
+    max_nodes  = 5
+  }
+}
