7f4f9ab64a5ea6006cbd83a5e1248a0afc34e79b
Manuel Morejon <manuel@mmorejon.io>
2020-02-17 13:11:38 +0100

    Exclude cilium images from flux image scan.
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,14 +6,13 @@ kubectl create ns flux-system
 # install flux
 helm upgrade -i flux fluxcd/flux --wait \
 --namespace flux-system \
+--version=1.2.0 \
 --set git.url=git@github.com:mmorejon/gitops-get-started \
---set git-branch=master \
---set git-path=namespaces \
---set git-poll-interval=5m \
---set sync-interval=5m \
---set manifest-generation=true \
---set memcached-hostname=flux-memcached.flux-system \
---set memcached-service=flux-memcached.flux-system
+--set git.branch=master \
+--set git.path=namespaces \
+--set git.pollInterval=5m \
+--set manifestGeneration=false \
+--set registry.excludeImage='k8s.gcr.io/*\,index.docker.io/cilium/*'
 
 # install flux-helm-operator
 helm upgrade -i helm-operator fluxcd/helm-operator --wait \
