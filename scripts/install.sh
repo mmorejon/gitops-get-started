3739ac8a2c97aab1a8028e8a2de1a0331181d1ed
Manuel Morejon <manuel@mmorejon.io>
2020-03-03 23:20:45 +0100

    Update parameters into install script.
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
 # create namespace
-kubectl create ns flux-system
+kubectl apply -f namespaces/flux-system/namespace.yaml
 
 # install flux
-helm upgrade -i flux fluxcd/flux --wait \
+helm upgrade flux fluxcd/flux --wait \
+--install \
 --namespace flux-system \
 --version=1.2.0 \
 --set git.url=git@github.com:mmorejon/gitops-get-started \
 --set git.branch=master \
 --set git.path=namespaces \
 --set git.pollInterval=5m \
+--set sync.interval=2m \
 --set manifestGeneration=false \
+--set registry.automationInterval=2m \
 --set registry.excludeImage="index.docker.io/*\,quay.io/*\,gcr.io/*" \
 --set syncGarbageCollection.enabled=true \
 --set syncGarbageCollection.dry=true
 
 # install flux-helm-operator
-helm upgrade -i helm-operator fluxcd/helm-operator --wait \
+helm upgrade helm-operator fluxcd/helm-operator --wait \
+--install \
 --namespace flux-system \
 --version=0.7.0 \
 --set createCRD=false \
